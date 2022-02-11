011421c30e52c2ebe5b19e3426b3702d0ae8a72e
omarsayed7 <sayedomar74@gmail.com>
2022-02-11 21:28:50 +0200

    added CORS to the API

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 from endpoints import blueprint as segmentation_endpoint
 from flask_ngrok import run_with_ngrok
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 run_with_ngrok(app)
 app.register_blueprint(segmentation_endpoint)
 
